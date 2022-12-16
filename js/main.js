44f20e31032bd89aa2dd87c0d2af0e7ea8738f99
AnyaSE <annseleznova@gmail.com>
2022-12-16 13:25:03 +0800

    added js code for the tasks 1 to 3

---
+++ js/main.js
@@ -0,0 +1,52 @@
+console.group("Вивести на сторінку в один рядок через кому числа від 10 до 20");
+
+let number = 10;
+let result = "";
+
+while(number < 21){
+    if (number === 20) {
+        result += number;
+    } else {
+        result+= `${number}, `;
+    }
+
+    number++;
+}
+
+console.log(result);
+console.groupEnd();
+
+console.group("Вивести квадрати чисел від 10 до 20");
+
+let i = 10; 
+
+while (i < 21) {
+    console.log(`${i} squared is equal to ${i * i}`);
+    i++;
+}
+
+console.groupEnd();
+
+console.group("Вивести таблицю множення на 7");
+
+let j = 1; 
+
+while (j < 11) {
+    console.log(`${j} * 7 = ${j * 7}`);
+    j++;
+}
+
+console.groupEnd();
+
+
+console.group("Знайти добуток усіх цілих чисел від 15 до 35");
+
+let a = 15;
+let b = 15; 
+
+while (a < 36 && b < 36) {
+    console.log (`${a} * ${b} = ${a * b}`);
+    a++;
+}
+
+
